ac55c408062138ddbd3e959cdccdefb7f0deeb86
DDRobinson-icstars <Drobinson@icstars.org>
2024-08-12 12:20:57 -0500

    Competion upload

---
+++ ControlFlow.cs
@@ -0,0 +1,52 @@
+using class Program
+{
+   
+    /// Task 1 variable ternary  
+    ///static int x = 50;
+
+
+   
+    private static void Main(string[] args)
+    {
+    ///Ternary Operator (Task 1)
+     /*   int score = x;
+        string grade =
+        score >= 90 ? "A" :
+        score >= 80 ? "B" :
+        score >= 70 ? "C" :
+        score >= 60 ? "D" :
+        "F";
+
+
+        Console.WriteLine($"Score: {score}, grade: {grade}");*/
+
+
+       
+    /// Switch Operation
+       /* int score = 50;
+        string grade;
+
+
+        switch (score)
+        {
+            case int n when (n >= 90):
+                grade = "A";
+                break;
+            case int n when (n >= 80):
+                grade = "B";
+                break;
+            case int n when (n >= 70):
+                grade = "C";
+                break;
+            case int n when (n >= 60):
+                grade = "D";
+                break;
+            default:
+                grade = "F";
+                break;
+        }
+
+
+        Console.WriteLine($"Score: {score}, Grade: {grade}");*/
+    }
+}
